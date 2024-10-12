ca1d702bcef66b71e1418344bbfe32a6af290227
kihong_kim <berserkppp1@gmail.com>
2024-10-12 16:14:23 +0900

    [feat] initialize

---
+++ src/test/java/com/kihong/grpc_practice/GrpcPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kihong.grpc_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
